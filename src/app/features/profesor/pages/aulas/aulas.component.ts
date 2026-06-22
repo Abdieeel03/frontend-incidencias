a19bf28f0ed01c315b1646ec68fb3001136e526a
Fran0808 <fortegaquipse@gmail.com>
2026-06-22 16:25:20 -0500

    feat: maquetar la vista de Aulas con datos de prueba

--- src/app/features/profesor/pages/aulas/aulas.component.ts
+++ src/app/features/profesor/pages/aulas/aulas.component.ts
@@ -1,9 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ProfesorHeaderComponent } from '../../components/profesor-header/profesor-header.component';
+
+interface Aula {
+  id: number;
+  nombre: string;
+  materia: string;
+  nivel: string;
+  alumnosCount: number;
+  criticasCount: number;
+  pendientesCount: number;
+}
 
 @Component({
   selector: 'app-aulas',
-  imports: [],
+  imports: [ProfesorHeaderComponent],
   templateUrl: './aulas.component.html',
   styleUrl: './aulas.component.css',
 })
-export class AulasComponent {}
+export class AulasComponent {
+  aulas = signal<Aula[]>([
+    {
+      id: 1,
+      nombre: '4to A',
+      materia: 'TUTORÍA',
+      nivel: 'Secundaria',
+      alumnosCount: 32,
+      criticasCount: 2,
+      pendientesCount: 1
+    },
+    {
+      id: 2,
+      nombre: '5to B',
+      materia: 'MATEMÁTICAS',
+      nivel: 'Secundaria',
+      alumnosCount: 28,
+      criticasCount: 0,
+      pendientesCount: 0
+    },
+    {
+      id: 3,
+      nombre: '6to A',
+      materia: 'CIENCIAS',
+      nivel: 'Primaria',
+      alumnosCount: 25,
+      criticasCount: 0,
+      pendientesCount: 3
+    }
+  ]);
+}
