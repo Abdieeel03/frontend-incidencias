6b6201b4add9dd4595e5e39675596de07c6cc63d
Fran0808 <fortegaquipse@gmail.com>
2026-06-22 15:33:37 -0500

    feat: componentes necesarios para profesor creados

---
+++ src/app/features/profesor/pages/incidencia-form/incidencia-form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-incidencia-form',
+  imports: [],
+  templateUrl: './incidencia-form.component.html',
+  styleUrl: './incidencia-form.component.css',
+})
+export class IncidenciaFormComponent {}
---
+++ src/app/features/profesor/pages/incidencias/incidencias.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-incidencias',
+  imports: [],
+  templateUrl: './incidencias.component.html',
+  styleUrl: './incidencias.component.css',
+})
+export class IncidenciasComponent {}
